7f4827f3c169be8872a3dbd350429c48ea594320
Keith-Hughes <mrkphughes@gmail.com>
2024-01-09 14:39:54 +0200

    combined js and html into one

--- app.js
+++ app.js
@@ -1,36 +1,3 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA1vCdvxRBKg4qiEvEiuaRBZXlRo80BlAU",
-  authDomain: "keithhughescv.firebaseapp.com",
-  projectId: "keithhughescv",
-  storageBucket: "keithhughescv.appspot.com",
-  messagingSenderId: "33187779200",
-  appId: "1:33187779200:web:996c27ce3e10615f648c65"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
-
-import { collection, addDoc } from "firebase/firestore"; 
-
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
 
