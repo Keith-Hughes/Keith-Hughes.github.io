f8baa4ae24530c268953ac623aebff2ee497f3e6
Keith-Hughes <mrkphughes@gmail.com>
2024-01-29 15:22:50 +0200

    Awards Section added

--- main.js
+++ main.js
@@ -55,8 +55,33 @@ function typeWriter(cursorID, className, txt) {
   typeCharacter();
 }
 
-const skipIntro = document.querySelector(".skip-intro");
-const mainContent = document.querySelector('.main-content');
+let slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
 
 
@@ -72,6 +97,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector('.menu-toggle');
     const menu = document.querySelector('.menu');
     const skipIntro = document.querySelector(".skip-intro");
+    showSlides(slideIndex);
+    skipIntro.style.opacity=1;
     skipIntro.addEventListener('click' , function () {                
         loadingScreen.style.opacity = 0;                // Listen for the 'transitionend' event to hide the loading screen after the fade-out transition
         loadingScreen.addEventListener('transitionend', function () {
@@ -115,6 +142,8 @@ document.addEventListener("DOMContentLoaded", function () {
         });
         
 });
+
+
 
 
 
